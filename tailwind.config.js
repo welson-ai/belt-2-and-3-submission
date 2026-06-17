ed03c8a7c5f993ec7063c8289c58885a5a60f417
h <metanexus8@gmail.com>
2026-06-18 02:02:57 +0300

    style: update Tailwind config with official Stellar brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        stellar: {
+          black: '#1d1d1f',
+          gold: '#b07c3b',
+          'gold-light': '#deb15e',
+          blue: '#283c62',
+          'blue-light': '#3d5180',
+          'blue-dark': '#1a2a45',
+          background: '#0f1419',
+          surface: '#1a1f26',
+        },
+      },
       animation: {
         'spin': 'spin 1s linear infinite',
       },
